6493c4861ffbb6732eb049c6bcbfb4aa9e1a6da3
StalinGD <p.zamorskiy@yandex.ru>
2023-11-02 21:31:00 +0300

    Added order details admin page

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.admin.views.decorators import staff_member_required
+from cart.cart import Cart
 from .models import OrderItem
 from .forms import OrderCreateForm
-from cart.cart import Cart
 from .tasks import order_created
+from .models import Order
 
 def order_create(request):
     cart = Cart(request)
@@ -22,3 +24,10 @@ def order_create(request):
         form = OrderCreateForm
         return render(request, 'orders/order/create.html',
             {'cart': cart, 'form': form})
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request,
+                  'admin/orders/order/detail.html',
+                  {'order': order})
